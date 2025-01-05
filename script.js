c924013f7e370d0a21d3dcdee9261ee3fa64d65e
João Eduardo Lima <joaoedulima@gmail.com>
2025-01-04 22:29:46 -0300

    Atualizando o JS para testar se a classe possui o light ou não, para trocar de escuro para claro.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function toggleMode() {
-    alert('Hello World');
+    const html = document.documentElement;
+
+    if(html.classList.contains('light')){
+        html.classList.remove('light');
+    }else{
+        html.classList.add('light');
+    }
+     
 
 }
